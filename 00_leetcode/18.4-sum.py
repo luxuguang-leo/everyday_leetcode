6690ab73cd0423ea5a3f47f986e21dbffbd8a3e0
Leo <luxuguangno1@163.com>
2019-07-30 00:59:24 +0800

    [0728]LC 26, 27 #Array, two pointers.

---
+++ 00_leetcode/18.4-sum.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=18 lang=python
+#
+# [18] 4Sum
+#
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+
---
+++ 00_leetcode/27.remove-element.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=27 lang=python
+#
+# [27] Remove Element
+#
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        #in place swap 
+        l, r = 0, len(nums) -1
+        while l <= r:
+            if nums[l] == val:
+                nums[l], nums[r], r= nums[r], nums[l], r -1
+            else:
+                l += 1
+        return l
+
