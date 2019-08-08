1ea96b3c9027735ad1fac9998fd54deed9b0041b
Leo <luxuguangno1@163.com>
2019-08-09 02:39:29 +0800

    [0809, LC141] Linked list, has circle, two pointers.

---
+++ 00_leetcode/141.linked-list-cycle.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=141 lang=python
+#
+# [141] Linked List Cycle
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        #fast and slow pointers
+        if not head:
+            return False
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True            
+        return False
+        
+
