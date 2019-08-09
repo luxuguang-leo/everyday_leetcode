e7603c01fabebbf66258e73e54757bdccf079812
Leo <luxuguangno1@163.com>
2019-08-09 10:45:01 +0800

    [0809 LC#203] Linked list, basic operation.

---
+++ 00_leetcode/203.remove-linked-list-elements.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=203 lang=python
+#
+# [203] Remove Linked List Elements
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        dummy = ListNode(0)
+        dummy.next = head
+        pre, cur = dummy, head
+        while cur:
+            if cur.val == val:
+                pre.next = cur.next
+            else:
+                pre = pre.next
+            cur = cur.next
+        return dummy.next
+            
+
