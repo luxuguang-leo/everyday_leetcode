b5d21ba801c6aa7c54da2b3f513763a12c4c9ec6
Leo <luxuguangno1@163.com>
2019-08-14 22:54:18 +0800

    [0814 LC#39] Backtracking, combination sum.

--- 00_leetcode/39.combination-sum.py
+++ 00_leetcode/39.combination-sum.py
@@ -4,11 +4,48 @@
 # [39] Combination Sum
 #
 class Solution(object):
+    def dfs(self, candidates, target, level, ret):
+        if target == 0:
+            ret.append(level)
+        for i in range(len(candidates)):
+            if target < candidates[i]:
+                return
+            self.dfs(candidates[i:], target - candidates[i], level+[candidates[i]], ret)
+        
+    def dfs_2(self, candidates, target, start, level, ret):
+        if target < 0:
+            return
+        if target == 0:
+            #ret.append(level)
+            ret.append(list(level))
+        for i in range(start, len(candidates)):
+            if target < candidates[i]:
+                return
+            #self.dfs_2(candidates, target - candidates[i], i, level+[candidates[i]], ret)
+            level.append(candidates[i])
+            self.dfs_2(candidates, target - candidates[i], i, level, ret)
+            level.pop()
     def combinationSum(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        
+        #method 1, 
+        '''
+        if not candidates:
+            return []
+        candidates.sort()
+        ret = []
+        self.dfs(candidates, target, [], ret)
+        return ret
+        '''
+        #method, pass index instead of slice avoiding copy
+        if not candidates:
+            return []
+        candidates.sort()
+        ret = []
+        self.dfs_2(candidates, target, 0,[], ret)
+        return ret
+
 
