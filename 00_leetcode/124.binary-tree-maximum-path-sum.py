9fe17a2964252171c2d0785406cd1b0c7c524f50
Leo <luxuguangno1@163.com>
2019-08-17 20:12:21 +0800

    [0817 #LC124] #Binary tree #recrusatively #maximum path sum.

---
+++ 00_leetcode/124.binary-tree-maximum-path-sum.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=124 lang=python
+#
+# [124] Binary Tree Maximum Path Sum
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+
