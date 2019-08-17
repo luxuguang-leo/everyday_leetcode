4eb47db5aab1034797b7422ef85f4bfca81d9178
Leo <luxuguangno1@163.com>
2019-08-17 18:06:07 +0800

    [0817 #LC78] #backtracking, all subsets.

--- 00_leetcode/78.subsets.py
+++ 00_leetcode/78.subsets.py
@@ -4,10 +4,19 @@
 # [78] Subsets
 #
 class Solution(object):
+    def dfs(self, nums, start, path, ret):
+        ret.append(path)
+        for i in range(start, len(nums)):
+            self.dfs(nums, i +1, path+[nums[i]], ret)
+    
     def subsets(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        if not nums:
+            return []
+        ret = []
+        self.dfs(nums, 0, [], ret)
+        return ret
 
