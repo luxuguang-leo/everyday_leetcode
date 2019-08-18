0071a40c34f1d783054b54ea76ceba8b6d3085b6
Leo <luxuguangno1@163.com>
2019-08-18 17:43:07 +0800

    [0818 #LC13] #string, roman to integer.

--- 00_leetcode/13.roman-to-integer.py
+++ 00_leetcode/13.roman-to-integer.py
@@ -9,5 +9,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        hash_set = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        #key point if hash_set[i] < hash_set[i+1], we can temporarily set -hast_set[i]
+        ret = 0
+        for i in range(len(s)-1):
+            if hash_set[s[i]] < hash_set[s[i+1]]:
+                ret -= hash_set[s[i]]
+            else:
+                ret += hash_set[s[i]]
+        ret += hash_set[s[-1]]
+        return ret
         
 
