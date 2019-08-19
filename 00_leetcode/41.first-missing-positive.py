b72750bffa2582de22e89f5ba329ebbcd599904a
Leo <luxuguangno1@163.com>
2019-08-20 02:18:03 +0800

    [0819 #LC41] #array, #bucket sort.

---
+++ 00_leetcode/41.first-missing-positive.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=41 lang=python
+#
+# [41] First Missing Positive
+#
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #time:nlg(n)
+        '''
+        nums.sort()
+        ret = 1
+        for n in nums:
+            if n == ret:
+                ret += 1
+        return ret
+        '''
+        #桶排序，大概思路是让对应的index都有对应的值，要不然swap
+        #3，4，-1，1按照排序应该是1，-1，3，4，暗含A[A[i]-1]=i
+        #如:A[A[0]-1] = A[0]=i+1, A[A[2]-1] = i+1=3
+        # ->3， 4， -1， 1
+        # -> -1, 4, 3, 1  swap(A[0], A[A[0]-1])
+        # -> -1, 1, 3, 4  swap(A[1],A[A[1]-1])
+        # -> 1, -1, 3, 4  swap(A[1], A[A[1] -1])
+        # -> 1, -1, 3, 4 不变，因为A[i] = i+1, i ==2
+        # -> 1, -1, 3, 4 不变，因为A[i] = i+1, i ==3
+        #通排序结束之后扫描，第一个非小于0的index+1
+        if not nums:
+            return 1
+        for i in range(len(nums)):
+            while nums[i] > 0 and nums[i] <= len(nums) and nums[nums[i]-1] != nums[i]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+        return len(nums)+1
+
