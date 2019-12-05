612a98f2d90d464d9c8be3d88e2dfdc0c83136c2
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-12-05 21:53:03 +0800

    [1205 #LC56]#intervals #merge.

--- 00_leetcode/56.merge-intervals.py
+++ 00_leetcode/56.merge-intervals.py
@@ -4,27 +4,28 @@
 # [56] Merge Intervals
 #
 
-class Intervals(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
+# @lc code=start
 class Solution(object):
     def merge(self, intervals):
         """
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        if not intervals:
-            return []
+        #sort,然后依次比较前一个pair和后一个pair
+        #1.有重合，并且后一个尾比较长，则更新交集区间为前pair的头和后pair的尾
+        #2.有重合，但前一个尾比较长，则前一个保持
+        #3.无重合， 将后一个pair入结果
+        if len(intervals) <= 1:
+            return intervals
         ret = []
-        intervals.sort(key = lambda x: x.start)
-        ret.append(Intevals(intervals[0])
-        for n in intervals:
-            pre = ret[-1]
-            if n.start <= pre.end:
-                pre.end = max(pre.end, n.end)
+        intervals.sort()
+        ret.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= ret[-1][1]:
+                ret[-1][-1] = max(ret[-1][-1], intervals[i][1])
             else:
-                ret.append(n)
+                ret.append(intervals[i])
         return ret
-
+        
+# @lc code=end
 
