ec9e03e8a92a1d800630127f1c84e64405de4388
绪光 <xuguang.lxg@alibaba-inc.com>
2019-12-11 00:05:01 +0800

    [1212 #rework #LC144]#binarytree #preorder #stack #dfs.

--- 00_leetcode/144.binary-tree-preorder-traversal.py
+++ 00_leetcode/144.binary-tree-preorder-traversal.py
@@ -12,10 +12,11 @@
 
 class Solution(object):
     def preorderHelper(self, root, ret):
-        if root:
-            ret.append(root.val)
-            self.preorderHelper(root.left, ret)
-            self.preorderHelper(root.right, ret)
+        if not root:
+            return
+        ret.append(root.val)
+        self.preorderHelper(root.left, ret)
+        self.preorderHelper(root.right, ret)
 
     def preorderTraversal(self, root):
         """
@@ -25,11 +26,11 @@ class Solution(object):
         #method 1
         '''
         ret = []
-        if root:
-            self.preorderHelper(root, ret)
+        self.preorderHelper(root, ret)
         return ret
         '''
-        #method 2
+        #method 2, not easy to understand
+        '''
         s, ret = [], []
         cur = root
         while s or cur:
@@ -39,6 +40,19 @@ class Solution(object):
                 cur = cur.left
             node = s.pop()
             cur = node.right
+        return ret
+        '''
+        #method 3, easy to understand, why append right node first?
+        #Because this is a stack, we need to visit 'left-node-right'
+        #So push right node into stack first.
+        stack, ret = [root], []
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            ret.append(node.val)
+            stack.append(node.right)
+            stack.append(node.left)
         return ret
         
 
