aa98ef4d049de4d91dc537db6e3f08a1ea0d3b9b
Leo <luxuguangno1@163.com>
2019-12-13 01:16:15 +0800

    [1212 #LC124]#tree #DFS #binary-tree-maximum-path-sum.

--- 00_leetcode/124.binary-tree-maximum-path-sum.py
+++ 00_leetcode/124.binary-tree-maximum-path-sum.py
@@ -11,10 +11,22 @@
 #         self.right = None
 
 class Solution(object):
+    def rootMax(self, root):
+        if not root:
+            return float('-inf')
+        l = max(0, self.rootMax(root.left))
+        r = max(0, self.rootMax(root.right))
+        self.ans = max(self.ans, l + r + root.val)
+        return root.val + max(l, r)
     def maxPathSum(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+        #DFS,考虑多种情况，对每一个节点左右求解取val+max(left, right, 0)
+        self.ans = float('-inf')
+        self.rootMax(root)
+        return self.ans
+        
         
 
