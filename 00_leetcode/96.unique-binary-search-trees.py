6551c6f2ff494be5fc9b155b931edc51a5248316
绪光 <xuguang.lxg@alibaba-inc.com>
2019-12-18 09:33:09 +0800

    [1218 #LC96]#DP #unique-binary-search-tree.

--- 00_leetcode/96.unique-binary-search-trees.py
+++ 00_leetcode/96.unique-binary-search-trees.py
@@ -13,11 +13,26 @@ class Solution(object):
         #f(0) = 1
         #f(n) = f(0)*f(n-1) + ...+f(n-1)*f(0)
         #最终数目是相加
+        '''
         dp = [0]*(n+1)
         dp[0] = 1
         for i in range(1, n+1):
             for j in range(0, i):
                 dp[i] += dp[j]*dp[i-j-1]
         return dp[-1]
+        '''
+        #refer to https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
+        #G(N):represent the soluction
+        #F(i, N):i as the root with all n numbers
+        #G(N) = F(0, N)+F(1,N)+...+F(N,N)
+        #F(i, N) = G(i-1)*G(N-i)  N>=i>=1 this is tricky,
+        #G(N) = G(0)*G(N-1) + G(1)*G(N-2)+...+G(N-1)*G(0)
+        DP = [0]*(n+1)
+        DP[0] = 1
+        for i in range(1, n+1):
+            for j in range(0, i):
+                DP[i] += DP[j]*DP[i-j-1]
+        print(DP)
+        return DP[-1]
         
 
