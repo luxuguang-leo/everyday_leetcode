7b3b86e9652176c336fe3abe7efbf9908d785ca6
Leo <luxuguangno1@163.com>
2020-01-04 17:21:14 +0800

    [@0104 #LC373 #007 #rework]#heapq #find-k-pair-with-smallest-sums.

--- 00_leetcode/373.find-k-pairs-with-smallest-sums.py
+++ 00_leetcode/373.find-k-pairs-with-smallest-sums.py
@@ -18,6 +18,7 @@ class Solution(object):
         #2.取出堆顶最小元素，放入结果中，然后看取出的元素的索引值i, j，因为第一个数组的每一个元素的可能性都已经放在堆中，所以需要
         #在第二个数组中尽可能组合，将(nums[i]+nums[j+1], i, j+1)继续放入堆中
         #循环的结束条件是返回值长度为k
+        '''
         if not nums1 or not nums2:
             return []
         ret = []
@@ -30,7 +31,44 @@ class Solution(object):
             if i +1 < len(nums1):
                 heapq.heappush(heap_list, (nums1[i+1] + nums2[j], i+1, j))
         return ret
+        '''
         
+        #brutal force??
+        '''
+        if not nums1 or not nums2:
+            return []
+        ret = []
+        min_heap = []
+        def push_to_heap(i, j):
+            if i < len(nums1) and j < len(nums2):
+                heapq.heappush(min_heap, (nums1[i]+nums2[j], i, j))
+        push_to_heap(0,0)
+        while len(ret) < min(k, len(nums1)*len(nums2)):
+            val, i, j = heapq.heappop(min_heap)
+            ret.append([nums1[i], nums2[j]])
+            push_to_heap(i, j+1)
+            if j == 0:
+                push_to_heap(i+1, j)
+        return ret
+        '''
+        #with set to record visited or not, very stright, no trick
+        if not nums1 or not nums2:
+            return []
+        ret, min_heap, visited = [], [], set()
+        heapq.heappush(min_heap, (nums1[0]+ nums2[0], (0, 0)))
+        visited.add((0, 0))
+        while len(ret) < min(k, len(nums1)*len(nums2)):
+            _, (i, j) = heapq.heappop(min_heap)
+            ret.append([nums1[i], nums2[j]])
+            if i+1 < len(nums1) and (i+1, j) not in visited:
+                heapq.heappush(min_heap, (nums1[i+1]+nums2[j], (i+1, j)))
+                visited.add((i+1, j))
+            if j+1 < len(nums2) and (i, j+1) not in visited:
+                heapq.heappush(min_heap, (nums1[i]+nums2[j+1], (i, j+1)))
+                visited.add((i, j+1))
+        return ret
+
+
         
 # @lc code=end
 
