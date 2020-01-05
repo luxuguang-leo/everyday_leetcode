a85ed35f97ecbc3c38e11aa0a2387f8967b6f3ea
绪光 <xuguang.lxg@alibaba-inc.com>
2020-01-05 21:23:06 +0800

    [@0105 #14 #rework #LC80]#array #two-pointers #remove-duplicates-from-sorted-array-ii.

--- 00_leetcode/80.remove-duplicates-from-sorted-array-ii.py
+++ 00_leetcode/80.remove-duplicates-from-sorted-array-ii.py
@@ -3,24 +3,22 @@
 #
 # [80] Remove Duplicates from Sorted Array II
 #
+
+# @lc code=start
 class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        #similar to previous #26, use two pointers, slow and fast
-        #key is to find when to replace the previous elements
-        #for #26, just need to satify A[fast] != A[slow]
-        #but for #80, we need to compare A[fast] != A[slow] or
-        #A[fast] == A[slow] and A[fast] != A[slow -1]
-        if len(nums) <= 2:
+        if len(nums) < 3:
             return len(nums)
-        slow = 1
-        for fast in range(2,len(nums)):
-            if nums[fast] != nums[slow] or nums[fast] == nums[slow] and nums[fast] != nums[slow-1]:
-                slow += 1
-                nums[slow] = nums[fast]
-        return slow+1
+        s = 1
+        for i in range(2, len(nums)):
+            if nums[i] != nums[s] or nums[i] == nums[s] and nums[i] != nums[s-1]:
+                s +=1
+                nums[s] = nums[i]
+        return s+1
         
+# @lc code=end
 
