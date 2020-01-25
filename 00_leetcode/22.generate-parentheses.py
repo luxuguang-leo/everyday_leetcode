a8b4d659fced50b5299f43f3990903b6492ed61c
Leo <luxuguangno1@163.com>
2020-01-26 01:53:31 +0800

    [@0125 #77-78 #LC22 #LC28] #string #DFS.

--- 00_leetcode/22.generate-parentheses.py
+++ 00_leetcode/22.generate-parentheses.py
@@ -4,13 +4,14 @@
 # [22] Generate Parentheses
 #
 class Solution(object):
-    def dfs(self, left, right, path, ret):
-        if left > 0:
-            self.dfs(left-1, right, path+'(', ret)
-        if right > 0 and right > left:
-            self.dfs(left, right-1, path + ')', ret)
-        if left == 0 and right == 0:
+    def dfs(self, left_remain, right_remain, path, ret):
+        if left_remain == 0 and right_remain == 0:
             ret.append(path)
+        if left_remain > 0:
+            self.dfs(left_remain-1, right_remain, path+'(', ret)
+        if right_remain > left_remain:
+            self.dfs(left_remain, right_remain-1, path + ')', ret)
+
     def generateParenthesis(self, n):
         """
         :type n: int
--- 00_leetcode/28.implement-str-str.py
+++ 00_leetcode/28.implement-str-str.py
@@ -10,10 +10,22 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        '''
         if not haystack and not needle: 
             return 0
         for i in range(len(haystack) - len(needle) + 1):
             if needle == haystack[i:i+len(needle)]:
+                return i
+        return -1
+        '''
+        if needle == "":
+            return 0
+        m, n = len(haystack), len(needle)
+        for i in range(m-n+1):
+            j = 0
+            while j < n and needle[j] == haystack[i+j]:
+                j +=1
+            if j == n:
                 return i
         return -1
 
