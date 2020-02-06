5c96c32bc1ab11c4e31770925803f72d2740d205
绪光 <xuguang.lxg@alibaba-inc.com>
2020-02-06 16:11:13 +0800

    [@0206 #124 #LC61]#linked-list #rotate-list.

--- 00_leetcode/61.rotate-list.py
+++ 00_leetcode/61.rotate-list.py
@@ -3,6 +3,8 @@
 #
 # [61] Rotate List
 #
+
+# @lc code=start
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -16,5 +18,33 @@ class Solution(object):
         :type k: int
         :rtype: ListNode
         """
+        #事实上不需要翻转链表，注意读题目
+        #find list size
+        size = 1
+        cur = head
+        while cur and cur.next:
+            cur = cur.next
+            size +=1
+        k = k % size if size else 0
+        if size <= 1 or k == 0:
+            return head
+        dummy = ListNode(-1)
+        dummy.next = head
+        slow = fast = dummy
+        for _ in range(k):
+            fast = fast.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        ret = cur = slow.next
+        while cur and cur.next:
+            cur = cur.next
+        cur.next = head
+        slow.next = None
+        return ret
         
+        
+
+
+# @lc code=end
 
