807bb6b8e5472648d22c3f178daab659e33687c0
Leo <luxuguangno1@163.com>
2020-02-06 22:56:51 +0800

    [@0206 #125 #LC82]#linked-list #remove-duplicates-from-sorted-list-ii.

--- 00_leetcode/82.remove-duplicates-from-sorted-list-ii.py
+++ 00_leetcode/82.remove-duplicates-from-sorted-list-ii.py
@@ -15,6 +15,7 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        '''
         dummy = ListNode(0)
         dummy.next, node = head, dummy
         while node.next  is not None and node.next.next is not None:
@@ -25,4 +26,22 @@ class Solution(object):
             else:
                 node = node.next
         return dummy.next
+        '''
+
+        if not head or not head.next:
+            return head
+        dummy = ListNode(float('inf'))
+        dummy.next = head
+        cur = dummy
+        while cur.next and cur.next.next:
+            #如果下一个节点和下下一个节点相等
+            #保存下一个节点值，然后往后遍历结点知道出现不相等的值
+            if cur.next.val == cur.next.next.val:
+                pre = cur.next.val
+                while cur.next and cur.next.val == pre:
+                    cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return dummy.next
+        
 
