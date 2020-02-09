8b7e6208f92146e34d8988cff5cf822b2f0be1eb
Leo <luxuguangno1@163.com>
2020-02-09 18:50:14 +0800

    [@0209 #140 #LC29]#binary-search #divide-two-integers.

--- 00_leetcode/29.divide-two-integers.py
+++ 00_leetcode/29.divide-two-integers.py
@@ -19,20 +19,22 @@ class Solution(object):
         digits = 0
         #find how many shitfs, 2^n*(1 or 0) +2^(n-1)*(1 or 0) 
         dividend, divisor = abs(dividend), abs(divisor)
-        while divisor <= dividend:
+        while divisor <= dividend:  #这里有==
             divisor <<=1
             digits +=1
-        divisor >>= digits
+        divisor >>= digits          #回归到原始divisor
         digits -=1
         ret = 0
-        while digits >= 0:
+        while digits >= 0:          #尝试从最大值开始减
             if dividend >= divisor << digits:
                 dividend -= divisor << digits
                 ret += 1<<digits
             digits -=1
         if sign:
-            return min(2147483647, ret)
+            #return min(2147483647, ret)
+            return min(2**31-1, ret)
         else:
-            return max(-2147483648, -ret)
+            #return max(-2147483648, -ret)
+            return max(-2**31, -ret)
 # @lc code=end
 
