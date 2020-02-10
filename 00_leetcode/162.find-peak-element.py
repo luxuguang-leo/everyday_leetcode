e14f7c717ecb7324cc116d8e16a78a463248ccc1
Leo <luxuguangno1@163.com>
2020-02-10 23:33:52 +0800

    [@0210 #150 #LC162 ]#binary-search #find-peak-element.

--- 00_leetcode/162.find-peak-element.py
+++ 00_leetcode/162.find-peak-element.py
@@ -11,16 +11,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # 1 2 3 4 5 4 3 2 1
+        '''
+        | 1 | 2 | 3 | 4 | 5 | 4 | 3 | 2 | 1 |
+        |---|---|---|---|---|---|---|---|---|
+        | l | _ | _ | _ | m | _ | _ | _ | r |
+        a[m] > a[m+1] -> r=m (Not m-1 since m is larger and it itself can be the answer)
+        | 1 | 2 | 3 | 4 | 5 | 4 | 3 | 2 | 1 |
+        |---|---|---|---|---|---|---|---|---|
+        | l | m | _ | _ | r | X | X | X | X |
+
+        a[m] < a[m+1] -> l = m+1 (Since m is smaller than m+1, m will for sure be not the answer)
+        | 1 | 2 | 3 | 4 | 5 | 4 | 3 | 2 | 1 |
+        |---|---|---|---|---|---|---|---|---|
+        | X | X | l | m | r | X | X | X | X |
+
+        a[m] < a[m+1] -> l = m+1 (Since m is smaller than m+1, m will for sure be not the answer)
+        | 1 | 2 | 3 | 4 | 5   | 4 | 3 | 2 | 1 |
+        |---|---|---|---|-----|---|---|---|---|
+        | X | X | X | X | l,r | X | X | X | X |
+
+        l is the answer
+        '''
         if not nums:
             return -1
-        l, r = 0, len(nums) -1
+        l, r = 0, len(nums)-1
         while l < r:
-            mid = (r-l)//2 + l
+            mid = l + (r-l)//2
             if nums[mid] < nums[mid+1]:
                 l = mid +1
-            else:
+            elif nums[mid] > nums[mid+1]:
                 r = mid
         return l
-        
-# @lc code=end
+       # @lc code=end
 
