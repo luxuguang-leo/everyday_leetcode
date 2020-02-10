3d54b8c151e13b1a8160c933beae8924c3938744
Leo <luxuguangno1@163.com>
2020-02-10 22:33:11 +0800

    [@0210 #147 #LC81]#binary-search #search-in-rotated-sorted-array-with-duplicate.

--- 00_leetcode/81.search-in-rotated-sorted-array-ii.py
+++ 00_leetcode/81.search-in-rotated-sorted-array-ii.py
@@ -14,23 +14,25 @@ class Solution(object):
         """
         if not nums:
             return False
-        l, r = 0, len(nums) -1
+        #考虑极限情况，nums[l], nums[mid], nums[r] 相等在一条直线上，无法区分，别的情况
+        #为何不考虑，别的情况可以区分有序数列，所以需要将前面水平的一段剔除掉
+        l, r = 0, len(nums)-1
         while l <= r:
-            mid = (r-l)//2 + l
+            mid = l + (r-l)//2
             if nums[mid] == target:
                 return True
             while l < mid and nums[l] == nums[mid]:
-                l += 1
+                l+=1
             if nums[l] <= nums[mid]:
-                if nums[l] <= target < nums[mid]:
+                if nums[l] <= target <= nums[mid]:
                     r = mid -1
                 else:
-                    l = mid + 1
+                    l = mid +1
             else:
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
+                if nums[mid] <= target <= nums[r]:
+                    l = mid +1
                 else:
-                    r = mid - 1
+                    r = mid -1
         return False
 # @lc code=end
 
