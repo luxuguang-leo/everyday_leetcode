ec78d1de7be2da6465b3a70bee6ed1b44776883b
Leo <luxuguangno1@163.com>
2020-02-11 23:40:32 +0800

    [@0211 #157 #LC315]#binary-search #count-of-smaller-after-self.

---
+++ 00_leetcode/315.count-of-smaller-numbers-after-self.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=315 lang=python
+#
+# [315] Count of Smaller Numbers After Self
+#
+
+# @lc code=start
+class Solution(object):
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        '''
+        #TLE
+        if not nums:
+            return []
+        ret = [0]*len(nums)
+        for i in range(len(nums)):
+            cnt = 0
+            for n in nums[i+1:]:
+                if n < nums[i]:
+                    cnt +=1
+            ret[i] = cnt
+        return ret
+        '''
+        #二分
+        ret, sorted_array = [], []
+        for i in range(len(nums)-1, -1, -1):
+            pos = bisect.bisect_left(sorted_array, nums[i])
+            ret.append(pos)
+            #bisect.insort_left(sorted_array, nums[i])
+            sorted_array.insert(pos, nums[i])
+        return ret[::-1]
+
+# @lc code=end
+
