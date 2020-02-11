5f06eaff96c443845e1ed582a93b312ed28bf860
Leo <luxuguangno1@163.com>
2020-02-12 00:00:48 +0800

    [@0211 #158 #LC349]#binary-search #intersection-of-two-arrays.

--- 00_leetcode/349.intersection-of-two-arrays.py
+++ 00_leetcode/349.intersection-of-two-arrays.py
@@ -15,6 +15,40 @@ class Solution(object):
         '''
         return list(set(nums1) & set(nums2))
         '''
+        #1.hashmap
+        #2.sort and compare
+        #return set(nums1)& set(nums2)
+        '''
+        if not nums1 or not nums2:
+            return []
+        nums1.sort()
+        nums2.sort()
+        i = j = 0
+        ret = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                ret.append(nums1[i])
+                i +=1
+                j +=1
+            elif nums1[i] < nums2[j]:
+                i +=1
+            else:
+                j+=1
+        return set(ret)
+        '''
+        #hashmap
+        if not nums1 or not nums2:
+            return []
+        ret, m = [], collections.defaultdict(int)
+        for n1 in nums1:
+            m[n1] +=1
+        for n2 in nums2:
+            if n2 in m:
+                ret.append(n2)
+        return set(ret)
+            
+            
+        
 
         
 # @lc code=end
