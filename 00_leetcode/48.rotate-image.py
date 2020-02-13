7a850fb7333f028987a6db2e04b910adb5f90751
Leo <luxuguangno1@163.com>
2020-02-14 00:18:41 +0800

    [@0214 #172 #LC48] #array #matrix #rotate-image.

--- 00_leetcode/48.rotate-image.py
+++ 00_leetcode/48.rotate-image.py
@@ -9,6 +9,7 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        '''
         if not matrix:
             return []
         n = len(matrix)
@@ -19,4 +20,15 @@ class Solution(object):
         for i in range(n//2):
             for j in range(n):
                 matrix[i][j], matrix[n-1-i][j] = matrix[n-1-i][j], matrix[i][j]
+        '''
+        #也可以先对折，再反转
+        N = len(matrix)
+        for i in range(N):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
+        for i in range(N):
+            for j in range(N/2):
+                matrix[i][j], matrix[i][N-1-j] = matrix[i][N-1-j], matrix[i][j]
+        return matrix
+        
 
