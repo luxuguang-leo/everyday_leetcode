cc8e6144a118f54e4fd0b1de57723ed8f2afdd26
Leo <luxuguangno1@163.com>
2020-02-14 00:00:15 +0800

    [@0213 #170-171 #LC45 #LC55] #array #greedy #jump game.

--- 00_leetcode/45.jump-game-ii.py
+++ 00_leetcode/45.jump-game-ii.py
@@ -11,14 +11,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        cur = cnt = pre = pos = 0
-        while cur < len(nums)-1:
+        #贪心算法，每走一步比较下一步能走的最远距离是多少，最终结果在最先到达的距离里面
+        #然后在下一步最远之间都要更新最远距离
+        pre_max_reach = cur_max_reach = 0
+        cnt = i = 0
+        while cur_max_reach < len(nums)-1:
+            while i <= pre_max_reach:
+                cur_max_reach = max(cur_max_reach, i+nums[i])
+                i +=1
+            if cur_max_reach == pre_max_reach:
+                return -1
+            pre_max_reach = cur_max_reach
             cnt +=1
-            pre = cur
-            while pos <= pre:
-                cur = max(pos+nums[pos], cur)
-                pos +=1
         return cnt
+                
 
 
 
--- 00_leetcode/55.jump-game.py
+++ 00_leetcode/55.jump-game.py
@@ -12,16 +12,27 @@ class Solution(object):
         :rtype: bool
         """
         #greedy，原题并不是求恰好到达i点,只求能否达到i点，并且i点在最大跳数之内，所以只要从前往后递推，i+g[i] >大于之前的距离就更新，否则不更新
-        n = len(nums)
-        if n <1:
-            return False
+        '''
+        if len(nums) <=1:
+            return True
         reach = 0
-        for i in range(n):
-            if i > reach or reach >= n-1:
+        for i in range(len(nums)):
+            if i > reach or reach > len(nums)-1:
                 break
             reach = max(reach, i + nums[i])
-        return reach >= n-1
-
+            i +=1
+        return reach >= len(nums)-1
+        '''
+        if len(nums) <=1:
+            return True
+        reach = 0
+        for i in range(len(nums)):
+            reach = max(reach, i + nums[i])
+            if reach < i+1:
+                return False
+            if reach >= len(nums)-1:
+                return True
+        return True
 
 
 
