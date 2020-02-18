3e9bf2cce9c99c713d72ce3639b71a7552ceea7f
Leo <luxuguangno1@163.com>
2020-02-18 23:41:08 +0800

    [@0218 #187-189 #LC169 #LC228 #LC229]#array #boyer-moore voting.

--- 00_leetcode/228.summary-ranges.py
+++ 00_leetcode/228.summary-ranges.py
@@ -11,22 +11,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        def append_str(l, r):
-            if l == r:
+        def convert2str(l, r):
+            if l > r:
+                return ""
+            elif l == r:
                 return str(l)
             else:
-                return str(l)+'->'+str(r)
+                return str(l)+"->"+str(r)
         if not nums:
             return []
-        ret, start, i = [], 0, 0
-        while i < len(nums) -1:
+        start = i = 0
+        end = len(nums)-1
+        ret = []
+        while i < end:
+            #tmp = []
             if nums[i]+1 != nums[i+1]:
-                ret.append(append_str(nums[start], nums[i]))
+                ret.append(convert2str(nums[start], nums[i]))
                 start = i+1
-            i +=1
-        ret.append(append_str(nums[start], nums[i]))
+            i+=1
+        ret.append(convert2str(nums[start], nums[i]))
+        #最终退出的时候i==len(nums)-1
+        #考虑两种情况，1.如果最后一个和之前是连续的，那么start为之前的一个数
+        #2.如果最后一个数不是连续的，那么start同样也是之前的一个数，没有差别
         return ret
-
-        
 # @lc code=end
 
