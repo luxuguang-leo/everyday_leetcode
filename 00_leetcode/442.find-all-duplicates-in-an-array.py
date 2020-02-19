0d34f55dd041da701b1a3918346617efedc254f3
Leo <luxuguangno1@163.com>
2020-02-20 01:07:11 +0800

    [0219 # #LC442 LC448]#array #find-all-duplicate in O(N) time and O(1) time. #in-place sort.

---
+++ 00_leetcode/442.find-all-duplicates-in-an-array.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=442 lang=python
+#
+# [442] Find All Duplicates in an Array
+#
+
+# @lc code=start
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        #method 1,使用如LC41,in-place交换的办法，将需要的数字交换到正确的位置上
+        '''
+        if not nums:
+            return nums
+        for i in range(len(nums)):
+            while nums[i] != i +1 and nums[nums[i]-1] != nums[i]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+        #ret = set()
+        ret = []
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                ret.append(nums[i])
+        return ret
+        '''
+        #method 2, 其实没必要交互排序，只需要将可以放在合适位置的数标记即可
+        ret = []
+        for i in range(len(nums)):
+            idx = abs(nums[i]) -1
+            if nums[idx] < 0:
+                ret.append(abs(nums[i]))
+            else:
+                nums[idx] = - nums[idx]
+        return ret
+        
+# @lc code=end
+
