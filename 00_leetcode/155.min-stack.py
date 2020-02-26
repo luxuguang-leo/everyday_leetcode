a4c8ed085228fc44ab51a81b5edf35041ecc69a1
绪光 <xuguang.lxg@alibaba-inc.com>
2020-02-26 15:35:32 +0800

    [0229 #200  #LC155]#stack  #min-stack.

--- 00_leetcode/155.min-stack.py
+++ 00_leetcode/155.min-stack.py
@@ -7,44 +7,93 @@
 # @lc code=start
 class MinStack(object):
 
+    '''
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.q = []
-        self.q_min = []
+        self.stack = []
+        self.minStack = []
+        
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
-        self.q.append(x)
-        if not self.q_min or x <= self.q_min[-1]:
-            self.q_min.append(x)
+        self.stack.append(x)
+        if not self.minStack or x <= self.minStack[-1]:
+            self.minStack.append(x)
+        
 
     def pop(self):
         """
         :rtype: None
         """
-        n = self.q.pop()
-        if n <= self.q_min[-1]:
-            self.q_min.pop()
-
+        if self.stack.pop() == self.minStack[-1]:
+            self.minStack.pop()
+        
+        
 
     def top(self):
         """
         :rtype: int
         """
-        return self.q[-1]
+        return self.stack[-1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
-        return self.q_min[-1]
+        return self.minStack[-1]
+    '''
+    #另外一种看上去比较简洁的方式是使用一个stack，但是保存正常stack和minStack的tuple,
+    #但是空间可能比第一种消耗更多
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        if not self.stack:
+            self.stack.append((x, x))
+        else:
+            self.stack.append((x, min(x, self.stack[-1][1])))
         
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack:
+            self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-1][0]
+        else:
+            return None
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-1][1]
+        else:
+            return None
 
 
 # Your MinStack object will be instantiated and called as such:
