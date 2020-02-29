6d459ce57b0a8d9c87ecf75286d5ff9c35762c24
Leo <luxuguangno1@163.com>
2020-02-29 14:35:03 +0800

    [0229 #binary-search ]#review.

--- binary_search.py
+++ binary_search.py
@@ -38,6 +38,23 @@ def binarySearchLeftBoundary(nums, target):
     else:
         return -1                   #否则也是没有找到
 
+    ##闭区间的写法
+def binartSearchLeftBoundary(nums, target):
+    if not nums:
+        return -1
+    l, r = 0, len(nums)-1
+    while l <= r:
+        mid = l + (r-l)//2
+        if target == nums[mid]:
+            r = mid-1
+        elif target < nums[mid]:
+            r = mid -1
+        elif target > nums[mid]:
+            l = mid +1
+    if l >= len(nums) or nums[l] != target: #找不到条件是l已经超出了数组范围，或者nums[l]并不是需要找的值
+        return -1
+    return l
+
 #patten 3
 #寻找区间右边界，也就是出现target的最后一个值，也就是右边界
 
@@ -52,7 +69,28 @@ def binarySearchRightBoundary(nums, target):
             r = mid
         elif target > nums[mid]:    #如果目标值大于中间值，那么左边界更细为mid+1
             l = mid +1
+    if left == 0:
+        return -1
     return left -1                  #如果推出则返回试探的值-1
+
+    ##闭区间写法
+    def binartSearchRightBoundary(nums, target):
+        if not nums:
+            return -1
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = l + (r-l)//2
+            if target == nums[mid]:
+                l = mid +1
+            elif target > nums[mid]:
+                l = mid +1
+            elif target < nums[mid]:
+                r = mid -1
+        #这里需要检查右边界越界的情况
+        if r < 0 or nums[r] != target:
+            return -1
+        return right
+
 
 
 例子：
@@ -62,7 +100,7 @@ target  l   r   mid    action
 8       3   4    3     target == nums[mid] ->返回3
 
 
-#2.寻找左边界  [1, 2, 2, 2, 3, 4, 5, 7] target = 2
+#2.1 使用开区间，寻找左边界  [1, 2, 2, 2, 3, 4, 5, 7] target = 2
 target  l   r   mid     action
 2       0   8   4       target < nums[mid] -> r = mid 
 2       0   4   2       target == nums[mid] -> r = mid = 2
@@ -70,12 +108,32 @@ target  l   r   mid     action
 2       0   1   0       target > nums[mid]  -> l = mid +1 = 1 -> l == r 循环条件推出
 这时候l = r = 1   l就是满足等于2的左边边界
 
-#寻找右边界   [1,   2,  3,  4,  4,  4,  4,  5]  target = 4
+#2.2 使用闭区间，寻找左边界  [1, 2, 2, 2, 3, 4, 5, 7] target = 2
+target  l   r   mid     actions
+2       0   7   3       target == nums[mid] -> r = mid -1
+2       0   2   1       target == nums[mid] -> r = mid -1 = 0 
+2       0   0   0       target > nums[mid]  -> l = mid +1 -> l > r 退出循环条件，并且l 并没有超出右边界
+这时候nums[l] == target, l就是左边界
+
+#3.1 使用开区间寻找右边界   [1,   2,  3,  4,  4,  4,  4,  5]  target = 4
 target  l   r   mid     action
 4       0   8   4       target = nums[mid] -> l = mid +1 = 5
 4       5   8   6       target = nums[mid] -> l = mid + 1 = 7  
 4       7   8   7       target < nums[mid] -> r = mid = 7  -> l == r 循环条件推出
 这时候返回值应该是l-1 = 6, 为右边界 
+
+#3.2 使用闭区间寻找右边界   [1,   2,  3,  4,  4,  4,  4,  5]  target = 4
+target  l   r   mid     action
+4       0   7   3       target == nums[mid] -> l = mid + 1
+4       4   7   5       target == nums[mid] -> l = mid + 1
+4       6   7   6       target == nums[mid] -> l = mid + 1
+4       7   7   7       target < nums[mid]  -> r = mid - 1 -> 6 ,这时候l > r退出循环条件，
+这时候r并没有小于数组的左边界(0)，并且nums[r] == target, r 就是要找的右边界 6
+
+#另外，可以直接使用bisect模块进行二分查找，
+#接口: bisect.bisect_left(a, n),在有序数组a中要插入n,如果已经存在n返回其左边位置, 
+#接口：bisect.bisect, bisect.bisect_right(a, n, lo = 0, hi = len(a)),在有序数组a中插入n,如果已经存在n,返回右边位置
+#接口：bisect.insortleft(a, n, lo = 0, hi = len(a)), 在有序数组a中插入n
 
 
 
