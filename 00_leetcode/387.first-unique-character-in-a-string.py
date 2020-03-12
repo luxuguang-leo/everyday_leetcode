eefcaf362827ce924085a19199e9871c7a3cbf49
Leo <luxuguangno1@163.com>
2020-03-13 00:17:59 +0800

    [0312 ] #2-questions #total-100 #interview-prepare.

--- 00_leetcode/387.first-unique-character-in-a-string.py
+++ 00_leetcode/387.first-unique-character-in-a-string.py
@@ -10,6 +10,7 @@ class Solution(object):
         :rtype: int
         """
         #hashmap, stupid question
+        '''
         hashTable = {}
         for ch in s:
             if ch not in hashTable:
@@ -20,6 +21,21 @@ class Solution(object):
             if hashTable[s[i]] == 1:
                 return i
         return -1
+        '''
+        #one-pass soluction, use map and dict
+        #@0312,使用dict记录次数，使用队列将出现一次的元素放入，如果发现大于1，则出队
+        hashTable = {}
+        q = collections.deque()
+        for idx, ch in enumerate(s):
+            hashTable[ch] = hashTable.get(ch, 0) +1
+            if hashTable[ch] == 1:
+                q.append((ch, idx))
+            while q and hashTable[q[0][0]] > 1:
+                q.popleft()
+        if not q:
+            return -1
+        else:
+            return q[0][1]
 
         
 
--- 00_leetcode/647.palindromic-substrings.py
+++ 00_leetcode/647.palindromic-substrings.py
@@ -13,19 +13,19 @@ class Solution(object):
         """
         if not s:
             return 0
-        def countPalind(s, l, r):
+        def cntPalindrome(str, l, r):
             cnt = 0
-            while l >=0 and r < len(s):
-                if s[l] == s[r]:
+            while l >=0 and r < len(str):
+                if str[l] == str[r]:
                     cnt +=1
-                    l -=1; r +=1
+                    l-=1;r+=1
                 else:
                     break
             return cnt
         ret = 0
         for i in range(len(s)):
-            ret += countPalind(s, i, i)
-            ret += countPalind(s, i, i+1)
+            ret += cntPalindrome(s, i, i)
+            ret += cntPalindrome(s, i, i+1)
         return ret
 # @lc code=end
 
