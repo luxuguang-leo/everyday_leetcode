eabf1251cfe48291e3010dfc66c1f6bd08fe25a4
Leo <luxuguangno1@163.com>
2020-04-12 13:24:47 +0800

    [0412 #LRU cache #system-design] #1-hard-questions]#total-43 #interview-preparation.

--- 00_leetcode/146.lru-cache.py
+++ 00_leetcode/146.lru-cache.py
@@ -74,6 +74,80 @@ class LRUCache(object):
         node.prev = p
         node.next = self.tail
         self.tail.prev = node
+
+    #another version is to insert from the head
+    '''
+class Node(object):
+    def __init__(self, key = 0, val = 0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.dict = dict()
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+        
+
+    def insert(self, node):
+        #insert from the head
+        next = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = next
+        next.prev = node
+        
+    def delete(self, node):
+        #delete node
+        pre = node.prev
+        next = node.next
+        pre.next = next
+        next.prev = pre
+        
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.dict:
+            #在字典中，取出值
+            #放在对头
+            n = self.dict[key]
+            self.delete(n)
+            self.insert(n)
+            return n.val
+        return -1
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key in self.dict:
+            #删除这个值
+            self.delete(self.dict[key])
+        n = Node(key, value)
+        self.insert(n)
+        self.dict[key] = n
+        if len(self.dict) > self.capacity:
+            #缓存溢出，找到队列尾部，删除之
+            node = self.tail.prev
+            self.delete(node)
+            del self.dict[node.key]
+
+    '''
+
         
 
 
